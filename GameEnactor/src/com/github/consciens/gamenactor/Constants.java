23c1631ea46c82a7098f6643f2140a4db4938557
Sergio Alvarez-Napagao <tranchis@gmail.com>
2011-01-31 18:03:01 +0100

    Java project and first code for game enacting

---
+++ GameEnactor/src/com/github/consciens/gamenactor/Constants.java
@@ -0,0 +1,6 @@
+package com.github.consciens.gamenactor;
+
+public final class Constants
+{
+	public static final int	SOCK_PORT	= 6969;
+}
---
+++ GameEnactor/src/com/github/consciens/gamenactor/GameBridge.java
@@ -0,0 +1,31 @@
+package com.github.consciens.gamenactor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GameBridge
+{
+	public static void main(String args[]) throws IOException
+	{
+		ServerSocket	ss;
+		Socket			s;
+		InputStream		is;
+		BufferedReader	br;
+		
+		ss = new ServerSocket(Constants.SOCK_PORT);
+		while(!ss.isClosed())
+		{
+			s = ss.accept();
+			is = s.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
+			while(!s.isClosed())
+			{
+				System.out.println(br.readLine());
+			}
+		}
+	}
+}
