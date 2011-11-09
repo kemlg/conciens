dd586140b341eae7760bdd76386308f381e7f37a
Sergio Alvarez-Napagao <tranchis@gmail.com>
2011-11-09 15:58:04 +0100

    fixed iWeb settings

---
+++ cOnscienS/Why_1_files/Why_1.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20111109
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-3,3,6,86),url:'Why_1_files/stroke_8.png'},{rect:new IWRect(-3,-3,6,6),url:'Why_1_files/stroke_9.png'},{rect:new IWRect(3,-3,222,6),url:'Why_1_files/stroke_10.png'},{rect:new IWRect(225,-3,6,6),url:'Why_1_files/stroke_11.png'},{rect:new IWRect(225,3,6,86),url:'Why_1_files/stroke_12.png'},{rect:new IWRect(225,89,6,6),url:'Why_1_files/stroke_13.png'},{rect:new IWRect(3,89,222,6),url:'Why_1_files/stroke_14.png'},{rect:new IWRect(-3,89,6,6),url:'Why_1_files/stroke_15.png'}],new IWSize(228,92)),stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,222),url:'Why_1_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'Why_1_files/stroke_1.png'},{rect:new IWRect(5,-5,321,10),url:'Why_1_files/stroke_2.png'},{rect:new IWRect(326,-5,10,10),url:'Why_1_files/stroke_3.png'},{rect:new IWRect(326,5,10,222),url:'Why_1_files/stroke_4.png'},{rect:new IWRect(326,227,10,11),url:'Why_1_files/stroke_5.png'},{rect:new IWRect(5,227,321,11),url:'Why_1_files/stroke_6.png'},{rect:new IWRect(-5,227,10,11),url:'Why_1_files/stroke_7.png'}],new IWSize(331,232))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Why_1_files/Why_1Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
