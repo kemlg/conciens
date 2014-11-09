53c6243cdf868b091a7229d921c78d1cc51093ae
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2014-11-09 22:18:57 +0100

    Add stress test to check server.

--- src/conciens/queries.clj
+++ src/conciens/queries.clj
@@ -17,37 +17,56 @@
                  :where {}
                  :sort {:millis 1})))))
 
-(def c (ref 0))
+(def c (ref {}))
+(def t (ref #{}))
+(def nums (ref {}))
 
 (defn message-handler
   [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
   #_(println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
                     (bson/decode payload) delivery-tag content-type type))
-  (dosync (alter c inc)))
+  (let [data (bson/decode payload)]
+    (dosync
+      (alter c
+             (fn [m]
+               (merge-with + m {(:app data) 1})))
+      (alter t
+             (fn [m]
+               (conj m (:timestamp data))))
+      (if (= (:app data) "EVENT_TYPE_CREATURE_UPDATE")
+        (alter nums
+               (fn [m]
+                 (merge-with + m {(:0 (:num-values data)) 1})))))))
 
-
-(let [conn  (rmq/connect {:hosts ["130.211.62.241"]})
-      ch    (lch/open conn)
-      qname "conciens.events"]
-  (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-  (lq/declare ch qname {:exclusive false :auto-delete true})
-  (lq/bind ch qname "amq.direct" {:routing-key "conciens.events"})
-  #_(lc/subscribe ch qname message-handler {:auto-ack true})
-  (lc/subscribe ch qname message-handler {:auto-ack true})
-  (Thread/sleep 2000)
-  (println "[main] Disconnecting...")
-  (println @c)
-  (rmq/close ch)
-  (rmq/close conn))
-
-#_(let [conn  (rmq/connect {:hosts ["130.211.62.241"]})
+(loop []
+(let [conn  (rmq/connect {:hosts ["127.0.0.1"]})
        ch    (lch/open conn)
-       qname "conciens.actions"]
-   (lq/declare ch qname {:exclusive false :auto-delete true})
+       qname "conciens.events"]
    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-   (lq/bind ch qname "amq.direct" {:routing-key "conciens.actions"})
-   (lb/publish ch "amq.direct" qname (bson/encode {:action-id "add-quest"})
-               {:content-type "application/bson"})
+   (lq/declare ch qname {:exclusive true :auto-delete true})
+   (lq/bind ch qname "amq.direct" {:routing-key "conciens.events"})
+   (lb/qos ch 20000)
+   #_(lc/subscribe ch qname message-handler {:auto-ack true})
+   (lc/subscribe ch qname message-handler {:auto-ack true})
+   (Thread/sleep 1000)
    (println "[main] Disconnecting...")
+   (clojure.pprint/pprint @c)
+   (clojure.pprint/pprint @nums)
    (rmq/close ch)
-   (rmq/close conn))
+   (rmq/close conn)
+   (count @t))
+
+(let [conn  (rmq/connect {:hosts ["127.0.0.1"]})
+      ch    (lch/open conn)
+      qname "conciens.actions"]
+  (lq/declare ch qname {:exclusive false :auto-delete true})
+  (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+  (lq/bind ch qname "amq.direct" {:routing-key "conciens.actions"})
+  (lb/publish ch "amq.direct" qname (bson/encode {:action-id "print-queue-size"})
+              {:content-type "application/bson"})
+  (println "[main] Disconnecting...")
+  (rmq/close ch)
+  (rmq/close conn)
+  (println (apply max @t)))
+  (recur))
+
